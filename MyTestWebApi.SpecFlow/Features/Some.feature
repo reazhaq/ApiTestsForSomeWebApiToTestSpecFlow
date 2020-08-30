c759713e55b1e2a3aace7c1f0a074388b57fecd9
reaz haq <reaz.haq@outlook.com>
2020-08-30 15:33:49 -0500

    init commit

---
+++ MyTestWebApi.SpecFlow/Features/Some.feature
@@ -0,0 +1,16 @@
+﻿Feature: Some
+	In order to ensure correct value is consumed
+	As a user
+	I want to validate value returned from api call
+
+@somefeature
+Scenario: Can call the API without any error
+	Given steady state
+	When I pass <value> to the end-point
+	Then I see <returnValue> in response
+
+	Examples: 
+		| value   | returnValue           |
+		| value1  | value1 was the param  |
+		| blah    | blah was the param    |
+		| hook'em | hook'em was the param |
---
+++ MyTestWebApi.SpecFlow/Steps/SomeSteps.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using RestSharp;
+using System.Net;
+using TechTalk.SpecFlow;
+//using Xunit.Abstractions;
+
+namespace MyTestWebApi.SpecFlow.Steps
+{
+    [Binding]
+    public class SomeSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        //private readonly ITestOutputHelper _testOutputHelper;
+        private readonly RestClient _api;
+        private IRestResponse<string> _response;
+
+        //curl -X GET "http://localhost:53134/api/Some?value=sdsd" -H "accept: */*"
+        public SomeSteps(ScenarioContext scenarioContext)//, ITestOutputHelper testOutputHelper)
+        {
+            _scenarioContext = scenarioContext;
+            //_testOutputHelper = testOutputHelper;
+            _api = new RestClient("http://localhost:53134/api/");
+        }
+
+        [Given]
+        public void Given_steady_state()
+        {
+        }
+
+        [When]
+        public void When_I_pass_VALUE_to_the_end_point(string value)
+        {
+            var request = new RestRequest("Some");
+            request.AddParameter("value", value);
+            //request.AddUrlSegment("someValue", value);
+            request.AddHeader("accept", "*/*");
+
+            _response = _api.Execute<string>(request);
+            //_testOutputHelper.WriteLine($"{_response}");
+        }
+
+        [Then]
+        public void Then_I_see_RETURNVALUE_in_response(string returnValue)
+        {
+            _response.StatusCode.Should().Be(HttpStatusCode.OK);
+            _response.Content.Should().NotBeNullOrEmpty()
+                .And.Be(returnValue);
+        }
+    }
+}
